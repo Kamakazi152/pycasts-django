7c03b982cbb8c9258f21a4492975dcd2260a110a
kamakazi152 <davenportjacob92@gmail.com>
2023-04-04 21:32:39 -0400

    Fixing typo in views.py to allow tests to pass to verify that the homepage is listing the episodes properly.

--- podcasts/views.py
+++ podcasts/views.py
@@ -10,5 +10,5 @@ class HomePageView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["episode"] = Episode.objects.filter().order_by("-pub_date")[:50]
+        context["episodes"] = Episode.objects.filter().order_by("-pub_date")[:50]
         return context
